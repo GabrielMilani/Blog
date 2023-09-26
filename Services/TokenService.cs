9ab7c5f7cdb84c1e8395289e60ad34ed36361032
Gabriel Milani <g.milani.e@gmail.com>
2023-09-25 22:14:46 -0300

    feito a organização da programs.cs e iniciado criação do serviço de envio de emails.

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,13 +13,10 @@ namespace Blog.Services
         {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new (ClaimTypes.Name, "gabriel"),
-                new (ClaimTypes.Role, "admin")
-            }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(6),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
